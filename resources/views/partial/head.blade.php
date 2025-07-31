baf79f12e82ea4c4199c66e3b638b4838a4b2ba3
dangLuan01 <luanboss012@gmail.com>
2025-07-31 16:36:59 +0700

    fix url statis

--- resources/views/partial/head.blade.php
+++ resources/views/partial/head.blade.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="{{url('assets/css/bootstrap-reboot.min.css')}}"/>
-<link rel="stylesheet" href="{{url('assets/css/bootstrap-grid.min.css')}}"/>
-<link rel="stylesheet" href="{{url('assets/css/magnific-popup.css')}}"/>
-<link rel="stylesheet" href="{{url('assets/css/select2.min.css')}}"/>
-<link rel="stylesheet" href="{{url('assets/css/admin.css')}}"/>
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap-reboot.min.css')}}"/>
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap-grid.min.css')}}"/>
+<link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}"/>
+<link rel="stylesheet" href="{{asset('assets/css/select2.min.css')}}"/>
+<link rel="stylesheet" href="{{asset('assets/css/admin.css')}}"/>
--- resources/views/partial/scripts.blade.php
+++ resources/views/partial/scripts.blade.php
@@ -1,7 +1,7 @@
-<script src="{{url('assets/js/jquery-3.5.1.min.js')}}"></script>
-<script src="{{url('assets/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{url('assets/js/jquery.magnific-popup.min.js')}}"></script>
-<script src="{{url('assets/js/smooth-scrollbar.js')}}"></script>
-<script src="{{url('assets/js/select2.min.js')}}"></script>
-<script src="{{url('assets/js/admin.js')}}"></script>
-<script src="{{url('assets/js/admin-function.js')}}"></script>
+<script src="{{asset('assets/js/jquery-3.5.1.min.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset('assets/js/smooth-scrollbar.js')}}"></script>
+<script src="{{asset('assets/js/select2.min.js')}}"></script>
+<script src="{{asset('assets/js/admin.js')}}"></script>
+<script src="{{asset('assets/js/admin-function.js')}}"></script>
